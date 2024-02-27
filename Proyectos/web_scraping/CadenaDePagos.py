1da527b93b9c1913dbab5c22e02da09c627f7150
Miguel Ortiz C <mortizcoilla@gmail.com>
2024-02-27 18:43:32 -0300

    Correccion de CadenaDePagos

--- Proyectos/web_scraping/CadenaDePagos.py
+++ Proyectos/web_scraping/CadenaDePagos.py
@@ -1,5 +1,7 @@
 # Análisis de Cadena de Pagos y Seguimiento
 import pandas as pd
+import plotly.express as px
+
 
 def leer_CDP(fecha):
     """
@@ -8,9 +10,8 @@ def leer_CDP(fecha):
     """
 
     nombre_archivo = f"Reporte Disconformidades PPagos I {fecha}.xlsx"
-    ruta_archivo = f"C:\\Users\\QV6522\\Workspace\\IngresosRegulados\\Proyectos\\BBDD\\{nombre_archivo}"  
-    
-    
+    ruta_archivo = f"C:\\Users\\QV6522\\Workspace\\IngresosRegulados\\Proyectos\\BBDD\\{nombre_archivo}"
+
     nombre_hoja = "MCP"
     try:
         df = pd.read_excel(ruta_archivo, sheet_name=nombre_hoja, engine='openpyxl')
@@ -18,6 +19,7 @@ def leer_CDP(fecha):
     except Exception as e:
         print(f"Error al leer el archivo Excel: {e}")
         return None
+
 
 def transformaciones_cdp(df):
     df.rename(columns={
@@ -36,7 +38,6 @@ def transformaciones_cdp(df):
     }, inplace=True)
 
     df['FCC'] = pd.to_datetime(df['FCC']).dt.date
-    df['MBIP'] = df['MBIP'].apply(lambda x: "${:,.2f}".format(x))
     df.drop(['NEM', 'MCP'], axis=1, inplace=True)
     df['RSA'] = df['RSA'].str.lower()
     df['RSD'] = df['RSD'].str.lower()
@@ -47,28 +48,31 @@ def transformaciones_cdp(df):
         ' sa': '',
         ' ltda': '',
         ' s.a.': '',
-        ',':'',
-        ' sa:':'',
-        ' limitada':'',
-        ' sa ':'',
-        ' spa ':'',
-        ' s.p.a.':'',
-        ' s.a':'',
-        '.':'',
-        ':':'',
-        '  ':''   
+        ',': '',
+        ' sa:': '',
+        ' limitada': '',
+        ' sa ': '',
+        ' spa ': '',
+        ' s.p.a.': '',
+        ' s.a': '',
+        '.': '',
+        ':': '',
+        '  ': ''
     }
-    
+
     for terminacion, correccion in terminaciones_dict.items():
         df['RSA'] = df['RSA'].str.replace(terminacion, correccion, regex=False)
         df['RSD'] = df['RSD'].str.replace(terminacion, correccion, regex=False)
-    
+
     return df
+
 
 def empresas(df):
     df['MBIP'] = df['MBIP'].replace('[\$,]', '', regex=True).astype(float)
-    deudores = df[['RD', 'RSD', 'MBIP', 'ND']].rename(columns={'RD': 'RUT', 'RSD': 'RS', 'MBIP': 'Deuda', 'ND': 'ND_Deuda'})
-    acreedores = df[['RA', 'RSA', 'MBIP', 'ND']].rename(columns={'RA': 'RUT', 'RSA': 'RS', 'MBIP': 'Crédito', 'ND': 'ND_Crédito'})
+    deudores = df[['RD', 'RSD', 'MBIP', 'ND']].rename(
+        columns={'RD': 'RUT', 'RSD': 'RS', 'MBIP': 'Deuda', 'ND': 'ND_Deuda'})
+    acreedores = df[['RA', 'RSA', 'MBIP', 'ND']].rename(
+        columns={'RA': 'RUT', 'RSA': 'RS', 'MBIP': 'Crédito', 'ND': 'ND_Crédito'})
     deudores['Deuda'] = deudores.groupby('RUT')['Deuda'].transform('sum')
     deudores['Q_Deuda'] = deudores.groupby('RUT')['ND_Deuda'].transform('nunique')
     acreedores['Crédito'] = acreedores.groupby('RUT')['Crédito'].transform('sum')
@@ -80,6 +84,7 @@ def empresas(df):
 
     return empresas_df
 
+
 def generar_resumen(df, rut_focal):
     df_filtrado = df[(df['RA'] == rut_focal) | (df['RD'] == rut_focal)].copy()
     df_filtrado['MBIP_limpio'] = df_filtrado['MBIP'].replace('[\$,]', '', regex=True).astype(float)
@@ -89,15 +94,19 @@ def generar_resumen(df, rut_focal):
         'Casos como Deudor': df_filtrado[df_filtrado['RD'] == rut_focal].shape[0],
         'Monto Total como Acreedor': df_filtrado[df_filtrado['RA'] == rut_focal]['MBIP_limpio'].sum(),
         'Monto Total como Deudor': df_filtrado[df_filtrado['RD'] == rut_focal]['MBIP_limpio'].sum(),
-        'Tipos de Disconformidad como Acreedor': df_filtrado[df_filtrado['RA'] == rut_focal]['TDC'].value_counts().to_dict(),
-        'Tipos de Disconformidad como Deudor': df_filtrado[df_filtrado['RD'] == rut_focal]['TDC'].value_counts().to_dict(),
+        'Tipos de Disconformidad como Acreedor': df_filtrado[df_filtrado['RA'] == rut_focal][
+            'TDC'].value_counts().to_dict(),
+        'Tipos de Disconformidad como Deudor': df_filtrado[df_filtrado['RD'] == rut_focal][
+            'TDC'].value_counts().to_dict(),
         'Contrapartes como Acreedor': df_filtrado[df_filtrado['RA'] == rut_focal]['RD'].value_counts().to_dict(),
         'Contrapartes como Deudor': df_filtrado[df_filtrado['RD'] == rut_focal]['RA'].value_counts().to_dict(),
     }
-    
+
     return resumen
 
+
 # ------------------------------------------------------------------------------------------------------------
+"""
 fecha = '202310'
 df = leer_CDP(fecha)
 
@@ -112,4 +121,32 @@ if df is not None:
         print(f"{key}: {value}")
 else:
     print("El DataFrame no pudo ser cargado. Por favor, verifica la ruta y el nombre del archivo.")
+"""
 # -----------------------------------------------------------------------------------------------------------
+# fecha = '202310'
+# df_original = leer_CDP(fecha)
+
+# if df_original is not None:
+#    df_transformado = transformaciones_cdp(df_original)
+#    df_empresas = empresas(df_transformado)
+
+#    ruta_salida = r"C:\Users\QV6522\Workspace\BBDD\01_Cadena De Pagos\Resumen_Empresas.xlsx"
+
+#    df_empresas.to_excel(ruta_salida, index=False)
+#    print(f"El archivo Excel ha sido exportado con éxito a {ruta_salida}")
+# else:
+#    print("No se pudo cargar el DataFrame original.")
+# -----------------------------------------------------------------------------------------------------------
+# fecha = "202310"
+# df_original = leer_CDP(fecha)
+# df_transformado = transformaciones_cdp(df_original)
+# ruta_exportacion_transformaciones = "C:\\Users\\QV6522\\Workspace\\BBDD\\01_Cadena De Pagos\\datos_transformados.xlsx"
+# df_transformado.to_excel(ruta_exportacion_transformaciones, index=False)
+# -----------------------------------------------------------------------------------------------------------
+
+df = leer_CDP("202310")
+df_transformado = transformaciones_cdp(df)
+deuda_total = df_transformado['MBIP'].sum()
+deuda_total_formateada = f"${deuda_total:,}"
+
+print(f"La deuda total del sistema es de {deuda_total_formateada} Millones de CLP")
--- Proyectos/web_scraping/macroeconomicos.py
+++ Proyectos/web_scraping/macroeconomicos.py
@@ -204,6 +204,13 @@ def itd_vatt(ruta_itd, fecha):
         print(f"No se encontraron datos para el periodo {fecha_inicio.strftime('%Y-%m')}")
         return df_itd
 
+    fecha_exacta = datetime.strptime(fecha, '%Y%m')
+    df_macro_exacto = df_macro[df_macro['Periodo'] == pd.to_datetime(fecha_exacta)]
+    if not df_macro_exacto.empty:
+        D_P = float(df_macro_exacto.iloc[0]['Dólar'])
+    else:
+        D_P = None
+
     IPC_0 = 97.89
     CPI_0 = 246.663
     D_0 = 629.55
@@ -218,13 +225,6 @@ def itd_vatt(ruta_itd, fecha):
 
     df_itd['D_0'] = D_0
     df_itd['D_k'] = float(df_macro_filtrado.iloc[0]['Dólar'])
-
-    fecha_exacta = datetime.strptime(fecha, '%Y%m')
-    df_macro_exacto = df_macro[df_macro['Periodo'] == pd.to_datetime(fecha_exacta)]
-    if not df_macro_exacto.empty:
-        D_P = float(df_macro_exacto.iloc[0]['Dólar'])
-    else:
-        D_P = None
     df_itd['D_P'] = D_P
 
     df_itd['CPI_k'] = float(df_macro_filtrado.iloc[0]['CPI'])
@@ -242,72 +242,100 @@ def itd_vatt(ruta_itd, fecha):
     df_itd['R_Ta'] = (1+Ta_k)/(1+Ta_0)
     df_itd['R_t'] = (t_k/t_0)*((1-t_0)/(1-t_k))
     """
-    
-    fecha_exacta = datetime.strptime(fecha, '%Y%m')
-    df_macro_exacto = df_macro[df_macro['Periodo'] == pd.to_datetime(fecha_exacta)]
-    if not df_macro_exacto.empty:
-        D_P = float(df_macro_exacto.iloc[0]['Dólar'])
-    else:
-        D_P = None
-    
+
     df_itd['AVI USD'] = df_itd['AVI US$'] * (df_itd['alfa'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
-                D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['beta'] * (
-                                              float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
-                                              (1 + Ta_k) / (1 + Ta_0)))
+            D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['beta'] * (
+                                                     float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
+                                                     (1 + Ta_k) / (1 + Ta_0)))
 
     df_itd['COMA USD'] = df_itd['COMA US$'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
-                D_0 / float(df_macro_filtrado.iloc[0]['Dólar']))
+            D_0 / float(df_macro_filtrado.iloc[0]['Dólar']))
 
     df_itd['AEIR USD'] = df_itd['AEIR US$'] * (df_itd['gama'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
-                D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['delta'] * (
-                                               float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
-                                               (1 + Ta_k) / (1 + Ta_0))) * ((t_k / t_0) * ((1 - t_0) / (1 - t_k)))
+            D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['delta'] * (
+                                                       float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
+                                                       (1 + Ta_k) / (1 + Ta_0))) * (
+                                     (t_k / t_0) * ((1 - t_0) / (1 - t_k)))
 
     df_itd['VATT USD'] = df_itd['AVI USD'] + df_itd['COMA USD'] + df_itd['AEIR USD']
-    
-    df_itd['AVI CLP'] = D_P * df_itd['AVI US$'] * (df_itd['alfa'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
+
+    df_itd['AVI CLP'] = D_P * df_itd['AVI US$'] * (
+                df_itd['alfa'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
                 D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['beta'] * (
-                                              float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
-                                              (1 + Ta_k) / (1 + Ta_0)))
+                        float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
+                        (1 + Ta_k) / (1 + Ta_0)))
 
     df_itd['COMA CLP'] = D_P * df_itd['COMA US$'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
-                D_0 / float(df_macro_filtrado.iloc[0]['Dólar']))
+            D_0 / float(df_macro_filtrado.iloc[0]['Dólar']))
 
-    df_itd['AEIR CLP'] = D_P * df_itd['AEIR US$'] * (df_itd['gama'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
+    df_itd['AEIR CLP'] = D_P * df_itd['AEIR US$'] * (
+                df_itd['gama'] * (float(df_macro_filtrado.iloc[0]['IPC']) / IPC_0) * (
                 D_0 / float(df_macro_filtrado.iloc[0]['Dólar'])) + df_itd['delta'] * (
-                                               float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
-                                               (1 + Ta_k) / (1 + Ta_0))) * ((t_k / t_0) * ((1 - t_0) / (1 - t_k)))
+                        float(df_macro_filtrado.iloc[0]['CPI']) / CPI_0) * (
+                        (1 + Ta_k) / (1 + Ta_0))) * ((t_k / t_0) * ((1 - t_0) / (1 - t_k)))
 
     df_itd['VATT CLP'] = df_itd['AVI CLP'] + df_itd['COMA CLP'] + df_itd['AEIR CLP']
-    
+
     return df_itd
 
-def zonal_y_dedicado():
-    ruta_archivo = "C:\\Users\\QV6522\\Workspace\\IngresosRegulados\\Proyectos\\BBDD\\Saldos Transmisión Zonal y Dedicado D7T 2311-def.xlsx"
-    
+
+def leer_datos_transmision_y_dedicado():
+    ruta_archivo_zonal_y_dedicado = ("C:\\Users\\QV6522\\Workspace\\IngresosRegulados\\Proyectos\\BBDD\\Saldos "
+                                     "Transmisión Zonal y Dedicado D7T 2311-def.xlsx")
+
+    ruta_archivo_nacional = ("C:\\Users\\QV6522\\Workspace\\IngresosRegulados\\Proyectos\\BBDD\\Saldos Transmisión "
+                             "Nacional D7T 2311-def.xlsx")
+
     # Leer Prorrata_Zonal
-    hoja_zonal = 'Prorrata_Zonal'
-    df_zonal = pd.read_excel(ruta_archivo, sheet_name=hoja_zonal, usecols='B:AA', skiprows=4)
+    df_zonal = pd.read_excel(ruta_archivo_zonal_y_dedicado, sheet_name='Prorrata_Zonal', usecols='B:AA', skiprows=4)
     df_zonal_filtrado = df_zonal[df_zonal['PROPIETARIO'] == 'ENGIE']
     df_zonal_filtrado['MES'] = pd.to_datetime(df_zonal_filtrado['MES'])
     df_zonal_filtrado = df_zonal_filtrado[df_zonal_filtrado['MES'] == '2023-11-01']
     df_zonal_filtrado = df_zonal_filtrado.sort_values(by='MES', ascending=False)
-    
+    df_zonal_filtrado = df_zonal_filtrado[['VATT [$]', 'ITE [$]', 'ITP [$]']]
+    df_zonal_filtrado['Sistema'] = 'zonal'
+
     # Leer Prorrata_Dedicado
-    hoja_dedicado = 'Prorrata_Dedicado'
-    df_dedicado = pd.read_excel(ruta_archivo, sheet_name=hoja_dedicado, usecols='B:U', skiprows=5)
+    df_dedicado = pd.read_excel(ruta_archivo_zonal_y_dedicado, sheet_name='Prorrata_Dedicado', usecols='B:U',
+                                skiprows=5)
     df_dedicado_filtrado = df_dedicado[df_dedicado['PROPIETARIO'] == 'ENGIE']
     df_dedicado_filtrado['MES'] = pd.to_datetime(df_dedicado_filtrado['MES'])
     df_dedicado_filtrado = df_dedicado_filtrado[df_dedicado_filtrado['MES'] == '2023-11-01']
     df_dedicado_filtrado = df_dedicado_filtrado.sort_values(by='MES', ascending=False)
-    
-    return df_zonal_filtrado, df_dedicado_filtrado
+    df_dedicado_filtrado = df_dedicado_filtrado[['VATT [$]', 'ITE [$]', 'ITP [$]']]
+    df_dedicado_filtrado['Sistema'] = 'dedicado'
+
+    # Leer Saldos 25T
+    df_25t = pd.read_excel(ruta_archivo_nacional, sheet_name='Saldos 25T', usecols='B:AL', skiprows=4)
+    df_25t_filtrado = df_25t[df_25t['PROPIETARIO'] == 'ETSA']
+    df_25t_filtrado['MES'] = pd.to_datetime(df_25t_filtrado['MES'])
+    df_25t_filtrado = df_25t_filtrado[df_25t_filtrado['MES'] == '2023-11-01']
+    df_25t_filtrado = df_25t_filtrado.sort_values(by='MES', ascending=False)
+    df_25t_filtrado = df_25t_filtrado[['VATT [$]', 'ITE [$]', 'ITP [$]']]
+    df_25t_filtrado['Sistema'] = '25T'
+
+    # Leer Saldos CU
+    df_cu = pd.read_excel(ruta_archivo_nacional, sheet_name='Saldos CU', usecols='B:S', skiprows=5)
+    df_cu_filtrado = df_cu[df_cu['PROPIETARIO'] == 'ETSA']
+    df_cu_filtrado['MES'] = pd.to_datetime(df_cu_filtrado['MES'])
+    df_cu_filtrado = df_cu_filtrado[df_cu_filtrado['MES'] == '2023-11-01']
+    df_cu_filtrado = df_cu_filtrado.sort_values(by='MES', ascending=False)
+    df_cu_filtrado = df_cu_filtrado[['VATT [$]', 'ITE [$]', 'ITP [$]']]
+    df_cu_filtrado['Sistema'] = 'CU'
+
+    # Concatenar todos los DataFrames filtrados
+    df_concatenado = pd.concat([df_zonal_filtrado, df_dedicado_filtrado, df_25t_filtrado, df_cu_filtrado],
+                               ignore_index=True)
+    cols = ['Sistema'] + [col for col in df_concatenado.columns if col != 'Sistema']
+    df_concatenado = df_concatenado[cols]
+
+    return df_concatenado
+
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # Llamada a la función y asignación de los resultados
-df_zonal, df_dedicado = zonal_y_dedicado()
-print(df_zonal.head())
-print(df_dedicado.head())
+df_total = leer_datos_transmision_y_dedicado()
+print(df_total)
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 # año_de_interés = 2013
 # df_indices_macroeconomicos = macroeconomicos(año_de_interés)
